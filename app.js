887efc8322b7a66cdaca406eda6225511a90cf55
sofi-figueroa <sfigueroa@udesa.edu.ar>
2024-03-23 16:28:38 -0300

    vista listadoBandas.ejs

--- app.js
+++ app.js
@@ -41,4 +41,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 module.exports = app;
